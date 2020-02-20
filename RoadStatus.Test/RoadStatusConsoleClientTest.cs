1d0804a3a4a334a5d9f7b40f115284ba13961da5
HUBSAPM <krish.venugopal@gmail.com>
2020-02-20 13:35:39 +0000

    refactored unit tests

--- RoadStatus.Test/RoadStatusConsoleClientTest.cs
+++ RoadStatus.Test/RoadStatusConsoleClientTest.cs
@@ -38,132 +38,87 @@ namespace RoadStatus.Test
         [Test]
         public async Task GivenValidRoadId_IsSpecified_WhenTheClientIsRunThen_TheRoadDisplayName_ShouldBeDisplayedAsync()
         {
-            using (StringWriter sw = new StringWriter())
-            {
-                Console.SetOut(sw);
+            _mockRoadStatusService.Setup(s => s.GetRoadStatusAsync("A2")).Returns(Task.FromResult(_validRoadStatus));
 
-                _mockRoadStatusService.Setup(s => s.GetRoadStatusAsync("A2")).Returns(Task.FromResult(_validRoadStatus));
+            var response = await _mockPrintService.Object.PrintRoadStatusResponseAsync("A2");
 
-                var response = await _mockPrintService.Object.PrintRoadStatusResponseAsync("A2");
+            string expected = $"The status of the {_validRoadStatus.DisplayName} is as follows:";
 
-                string expected = $"The status of the {_validRoadStatus.DisplayName} is as follows:";
-
-                Assert.IsTrue(_mockPrintService.Object.GetOutputMessage().Contains(expected));
-            }
+            Assert.IsTrue(_mockPrintService.Object.GetOutputMessage().Contains(expected));
         }
 
         [Test]
         public async Task GivenValidRoadId_IsSpecified_WhenTheClientIsRunThen_TheStatusSeverity_ShouldBeDisplayedAsync()
         {
-            using (StringWriter sw = new StringWriter())
-            {
-                Console.SetOut(sw);
+            _mockRoadStatusService.Setup(s => s.GetRoadStatusAsync(It.IsAny<string>())).Returns(Task.FromResult(_validRoadStatus));
 
-                var mockRoadStatusService = new Mock<IRoadStatusService>();
-                mockRoadStatusService.Setup(s => s.GetRoadStatusAsync(It.IsAny<string>())).Returns(Task.FromResult(_validRoadStatus));
+            var response = await _mockPrintService.Object.PrintRoadStatusResponseAsync("A2");
 
-                RoadStatusPrinter printer = new RoadStatusPrinter(mockRoadStatusService.Object);
+            string expected = $"Road Status is {_validRoadStatus.StatusSeverity}";
 
-                var response = await printer.PrintRoadStatusResponseAsync("A2");
-
-                string expected = $"Road Status is {_validRoadStatus.StatusSeverity}";
-
-                Assert.IsTrue(sw.ToString().Contains(expected));
-            }
+            Assert.IsTrue(_mockPrintService.Object.GetOutputMessage().Contains(expected));
         }
 
         [Test]
         public async Task GivenValidRoadId_IsSpecified_WhenTheClientIsRunThen_TheStatusSeverityDescription_ShouldBeDisplayedAsync()
         {
-            using (StringWriter sw = new StringWriter())
-            {
-                Console.SetOut(sw);
+            _mockRoadStatusService.Setup(s => s.GetRoadStatusAsync(It.IsAny<string>())).Returns(Task.FromResult(_validRoadStatus));
 
-                var mockRoadStatusService = new Mock<IRoadStatusService>();
-                mockRoadStatusService.Setup(s => s.GetRoadStatusAsync(It.IsAny<string>())).Returns(Task.FromResult(_validRoadStatus));
+            var response = await _mockPrintService.Object.PrintRoadStatusResponseAsync("A2");
 
-                RoadStatusPrinter printer = new RoadStatusPrinter(mockRoadStatusService.Object);
+            string expected = $"Road Status Description is {_validRoadStatus.StatusSeverityDescription}";
 
-                var response = await printer.PrintRoadStatusResponseAsync("A2");
-
-                string expected = $"Road Status Description is {_validRoadStatus.StatusSeverityDescription}";
-
-                Assert.IsTrue(sw.ToString().Contains(expected));
-            }
+            Assert.IsTrue(_mockPrintService.Object.GetOutputMessage().Contains(expected));
         }
 
         [Test]
         public async Task GivenInValidRoadId_IsSpecified_WhenTheClientIsRunThen_TheClientIsRun_ThenApplicationShouldDisplay_InformativeErrorAsync()
         {
-            using (StringWriter sw = new StringWriter())
+            _mockRoadStatusService.Setup(s => s.GetRoadStatusAsync(It.IsAny<string>())).Throws(new ApiException
             {
-                Console.SetOut(sw);
-
-                var mockRoadStatusService = new Mock<IRoadStatusService>();
-                mockRoadStatusService.Setup(s => s.GetRoadStatusAsync(It.IsAny<string>())).Throws(new ApiException
+                StatusCode = 404,
+                Error = new ApiErrorResponse
                 {
-                    StatusCode = 404,
-                    Error = new ApiErrorResponse
-                    {
-                        HttpStatus = "NotFound",
-                        HttpStatusCode = 404,
-                        Message = "The following road id is not recognised: A233"
-                    }
-                });
+                    HttpStatus = "NotFound",
+                    HttpStatusCode = 404,
+                    Message = "The following road id is not recognised: A233"
+                }
+            });
 
-                RoadStatusPrinter printer = new RoadStatusPrinter(mockRoadStatusService.Object);
+            var response = await _mockPrintService.Object.PrintRoadStatusResponseAsync("A233");
 
-                var response = await printer.PrintRoadStatusResponseAsync("A233");
+            string expected = "A233 is not a valid road";
 
-                string expected = "A233 is not a valid road";
-
-                Assert.IsTrue(sw.ToString().Contains(expected));
-            }
+            Assert.IsTrue(_mockPrintService.Object.GetOutputMessage().Contains(expected));
         }
 
         [Test]
         public async Task GivenValidRoadId_IsSpecifiedWhenTheClientIsRun_ThenShouldReturnZeroSystemSuccessCodeAsync()
         {
-            using (StringWriter sw = new StringWriter())
-            {
-                Console.SetOut(sw);
+            _mockRoadStatusService.Setup(s => s.GetRoadStatusAsync(It.IsAny<string>())).Returns(Task.FromResult(_validRoadStatus));
 
-                var mockRoadStatusService = new Mock<IRoadStatusService>();
-                mockRoadStatusService.Setup(r => r.GetRoadStatusAsync(It.IsAny<string>())).Returns(Task.FromResult(_validRoadStatus));
+            var result = await _mockPrintService.Object.PrintRoadStatusResponseAsync("A2");
 
-                RoadStatusPrinter printer = new RoadStatusPrinter(mockRoadStatusService.Object);
-
-                var result = await printer.PrintRoadStatusResponseAsync("A2");
-
-                Assert.AreEqual(result, 0);
-            }
+            Assert.AreEqual(result, 0);
         }
 
         [Test]
         public async Task GivenInvalidRoadIdIsSpecifiedWhenTheClientIsRunThenShouldReturnNonZeroSystemErrorCodeAsync()
         {
-            using (StringWriter sw = new StringWriter())
+            _mockRoadStatusService.Setup(s => s.GetRoadStatusAsync(It.IsAny<string>())).Throws(new ApiException
             {
-                Console.SetOut(sw);
-
-                var mockRoadStatusService = new Mock<IRoadStatusService>();
-                mockRoadStatusService.Setup(r => r.GetRoadStatusAsync(It.IsAny<string>())).Throws(new ApiException
+                StatusCode = 404,
+                Error = new ApiErrorResponse
                 {
-                    StatusCode = 404,
-                    Error = new ApiErrorResponse
-                    {
-                        HttpStatus = "NotFound",
-                        HttpStatusCode = 404,
-                        Message = "The following road id is not recognised: A233"
-                    }
-                });
+                    HttpStatus = "NotFound",
+                    HttpStatusCode = 404,
+                    Message = "The following road id is not recognised: A233"
+                }
+            });
 
-                RoadStatusPrinter printer = new RoadStatusPrinter(mockRoadStatusService.Object);
+            var result = await _mockPrintService.Object.PrintRoadStatusResponseAsync("A2");
 
-                var result = await printer.PrintRoadStatusResponseAsync("A233");
-
-                Assert.AreEqual(result, 1);
-            }
+            Assert.AreEqual(result, 1);
         }
     }
 }
