193a154e7314290068c9788aa180a7669516e369
HUBSAPM <krish.venugopal@gmail.com>
2020-02-20 09:57:03 +0000

    Added PrinService to Autofac

--- RoadStatus.Client/Autofac/Modules/RoadStatusModules.cs
+++ RoadStatus.Client/Autofac/Modules/RoadStatusModules.cs
@@ -13,6 +13,9 @@ namespace RoadStatus.Client.Autofac.Modules
             builder.Register(c => new RoadStatusService(new HttpClientHandler(), c.Resolve<IConfig>()))
                    .As<IRoadStatusService>();
 
+            builder.Register(c => new PrintService(new RoadStatusService(new HttpClientHandler(), c.Resolve<IConfig>())))
+                .As<IPrintService>();
+
             base.Load(builder);
         }
     }
